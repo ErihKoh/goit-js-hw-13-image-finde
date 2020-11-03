28e9166f4cca6f131a25384b0f4c7b357037fbc3
Yurii Zubar <barsuk0831@gmail.com>
2020-11-03 21:41:04 +0200

    минимально работающий запрос

---
+++ src/data/gallery_pixabay.js
@@ -0,0 +1,26 @@
+
+import galleryImagesTpl from '../templates/gallery_image.hbs';
+import API from './apiService';
+import refs from './refs';
+// import '@pnotify/core/dist/BrightTheme.css';
+// import '@pnotify/core/dist/PNotify.css';
+// import { error } from '@pnotify/core';
+// import { defaults } from '@pnotify/core';
+
+const debounce = require('lodash.debounce');
+
+refs.searchForm.addEventListener('input', debounce(onSearch, 500));
+
+function onSearch(e) {
+    e.preventDefault();
+    const seurchName = e.target.value;
+    // console.log(seurchName);
+    API.apiServise(seurchName).then(data => {
+        const markup = galleryImagesTpl(data.hits);
+        // console.log(data.hits)
+        // console.log(markup);
+       return refs.gallery.insertAdjacentHTML('beforeend', markup);
+    });
+
+  
+}
